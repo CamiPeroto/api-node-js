f40745c30574aad2355232ccdb16ba64d47795f2
CamiPeroto <camilabuenoperoto@gmail.com>
2025-03-25 14:19:56 -0300

    Projeto base

---
+++ dist/index.js
@@ -0,0 +1,15 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+//criar aplicação express
+const app = (0, express_1.default)();
+//criar a rota get principal
+app.get("/", (req, res) => {
+    res.send('Bem-vinda, Camila');
+});
+app.listen(8080, () => {
+    console.log('Servidor iniciado na porta 8080: http://localhost:8080');
+});
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, {Request, Response} from "express"
+//criar aplicação express
+const app = express()
+//criar a rota get principal
+app.get("/", (req: Request, res: Response) => {
+    res.send('Bem-vinda, Camila')
+})
+
+app.listen(8080, () =>{
+console.log('Servidor iniciado na porta 8080: http://localhost:8080')
+})
