a6cbf871910b577edcb6891d70a9132108cc3d06
CamiPeroto <camilabuenoperoto@gmail.com>
2025-04-01 11:12:46 -0300

    implementando a foreign key nas tabelas

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,5 +1,12 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
+//importar biblioteca com as variáveis de ambiente
+import dotenv from "dotenv"
+
+
+//carregar as variáveis do arquivo.env
+dotenv.config()
+
 
 const dialect = process.env.DB_DIALECT ?? "mysql"
 
@@ -14,5 +21,5 @@ export const AppDataSource = new DataSource({
     logging: true,
     entities: [],
     subscribers: [],
-    migrations: [],
+    migrations: [__dirname + "/migration/*.js"],
 })
