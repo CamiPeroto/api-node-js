e21ab94e5e79fb0206bfa06c4d89b3888dc49d02
CamiPeroto <camilabuenoperoto@gmail.com>
2025-04-08 19:03:47 -0300

    Configuração do cors para permitir requisições externas a api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import express from "express";
 //importar biblioteca variáveis de ambiente
+
+//Importar a biblioteca para permitir requisições externas a api
+import cors from "cors";
+
 import dotenv from "dotenv";
 
 //carregar variáveis do arquivo .env
@@ -9,19 +13,20 @@ dotenv.config();
 const app = express();
 
 //Criar o middleware para receber os dados no corpo da requisição
-app.use(express.json())
-
+app.use(express.json());
+//Criar o middleware para permitir requisições externas 
+app.use(cors());
 //incluir as controllers
-import AuthController from "./controllers/AuthController"
-import SituationsController from "./controllers/SituationsController"
-import ProductSituationsController from "./controllers/ProductSituationsController"
-import ProductCategoriesController from "./controllers/ProductCategoriesController"
+import AuthController from "./controllers/AuthController";
+import SituationsController from "./controllers/SituationsController";
+import ProductSituationsController from "./controllers/ProductSituationsController";
+import ProductCategoriesController from "./controllers/ProductCategoriesController";
 
 //criar as rotas
-app.use('/', AuthController)
-app.use('/', SituationsController)
-app.use('/', ProductSituationsController);
-app.use('/', ProductCategoriesController);
+app.use("/", AuthController);
+app.use("/", SituationsController);
+app.use("/", ProductSituationsController);
+app.use("/", ProductCategoriesController);
 
 app.listen(process.env.PORT, () => {
   console.log(`Servidor iniciado na porta ${process.env.PORT}: http://localhost:${process.env.PORT}`);
